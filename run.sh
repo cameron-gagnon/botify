fa681859dcddec1d35bb9a196f7cf966f4974428
Cameron Gagnon <cameron.gagnon@gmail.com>
2019-05-31 10:37:47 -0700

    Add requirements.txt after re-setting up with a virtualenv

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 echo "Killing any previous botifies..."
 
-echo `pkill python3.5`
+echo `pkill python`
 
 echo "Starting botify"
 sleep 2
-nohup ./main.py  &
+nohup ./main.py &
 sleep 2
 echo "Started botify"
