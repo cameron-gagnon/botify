60e69ffedf1b2e98de72ab007dde181dd2865c62
Cameron Gagnon <cameron.gagnon@gmail.com>
2020-01-25 14:04:34 -0800

    Don't kill previous python processes
    ignore spotipy's credential cache file: `.cache-<EMAIL>`

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo "Killing any previous botifies..."
-
-echo `pkill python`
-
 echo "Starting botify"
-sleep 2
+
 source venv/bin/activate
 nohup python ./main.py 2>&1 &
 deactivate
-sleep 2
+
 echo "Started botify"
