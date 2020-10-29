1eb2fc88dfb35f3cb2b847f791c2269683671f06
Cameron Gagnon <cameron.gagnon@gmail.com>
2020-10-29 00:03:24 -0700

    truncate debug and error logs on start; kill previous botifies
    Also print out process to ensure it is running at the end of
    running run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-echo "Starting botify"
+truncate_file() {
+    if [[ ! -e $1 ]]; then
+        echo "File $1 doesn't exist. Not truncating..."
+        return
+    fi
+
+    tail -n 5000 $1 > ${1}.bak
+    mv ${1}.bak $1
+}
+
+set -e echo "Starting botify"
+
+echo `pkill -f botify`
+
+truncate_file debug.log
+truncate_file error.log
 
 source venv/bin/activate
 nohup python ./main.py 2>&1 &
 deactivate
 
 echo "Started botify"
+
+ps aux | grep -v grep | grep botify
