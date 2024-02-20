dcc1d8d89baaa1701d3825b5874cc6f7f4faf0f1
Cameron Gagnon <cameron.gagnon@gmail.com>
2024-02-19 20:26:43 -0800

    emit errors to debug log instead of separate error log

--- app/helpers/errors/handlers.py
+++ app/helpers/errors/handlers.py
@@ -6,8 +6,8 @@ from app.helpers.errors import bp as error_bp
 # handles all errors
 @error_bp.app_errorhandler(Exception)
 def all_errors(error):
-    with open('errors.log', 'a') as ef:
+    with open('debug.log', 'a') as ef:
         ef.write(traceback.format_exc())
         ef.write("="*100)
-        ef.write("\n\n")
+        ef.write("\n")
     return 'An error occurred, yell at stroopC', 500
